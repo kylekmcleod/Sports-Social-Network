2a95362c94757021d2a1227ce1fa8e20c654671f
kylekmcleod <kylekmcleod1@gmail.com>
2025-03-15 14:04:28 -0700

    Better navbar auth access

--- assets/components/leftSideBar.php
+++ assets/components/leftSideBar.php
@@ -12,25 +12,32 @@ include_once('../src/controllers/auth.php');
             <img src="../assets/svg/explore.svg" class="sidebar-menu__item-icon" />
             <span>Explore</span>
         </a>
-        <a href="" class="sidebar-menu__item">
-            <img src="../assets/svg/notifications.svg" class="sidebar-menu__item-icon" />
-            <span>Notifications</span>
-        </a>
-        <a href="profile.php" class="sidebar-menu__item">
-            <img src="../assets/svg/profile.svg" class="sidebar-menu__item-icon" />
-            <span>Profile</span>
-        </a>
-        <a href="settings.php" class="sidebar-menu__item">
-            <img src="../assets/svg/setting.svg" class="sidebar-menu__item-icon" />
-            <span>Settings</span>
-        </a>
 
         <?php
         if (checkIfLoggedIn()) {
             ?>
+            <a href="" class="sidebar-menu__item">
+                <img src="../assets/svg/notifications.svg" class="sidebar-menu__item-icon" />
+                <span>Notifications</span>
+            </a>
+            <a href="profile.php" class="sidebar-menu__item">
+                <img src="../assets/svg/profile.svg" class="sidebar-menu__item-icon" />
+                <span>Profile</span>
+            </a>
+            <a href="settings.php" class="sidebar-menu__item">
+                <img src="../assets/svg/setting.svg" class="sidebar-menu__item-icon" />
+                <span>Settings</span>
+            </a>
             <a href="logout.php" class="sidebar-menu__item">
                 <img src="../assets/svg/logout.svg" class="sidebar-menu__item-icon" />
                 <span>Logout</span>
+            </a>
+            <?php
+        } else {
+            ?>
+            <a href="login.php" class="sidebar-menu__item">
+                <img src="../assets/svg/login.svg" class="sidebar-menu__item-icon" />
+                <span>Login</span>
             </a>
             <?php
         }
--- public/logout.php
+++ public/logout.php
@@ -3,6 +3,6 @@ session_start();
 
 session_unset();
 session_destroy();
-header("Location: /COSC360/public/login.php");
+header("Location: /COSC360/public/homepage.php");
 exit();
 ?>
